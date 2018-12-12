621795fe356abf884a8b88f8b96223115d609e41
sushant2030 <sushantalone@Sushants-Mac-mini.local>
2018-12-13 02:14:51 +0530

    Change Password Service Created!!!

---
+++ Trackzia/NetworkServices/ChangePasswordService.swift
@@ -0,0 +1,72 @@
+//
+//  ChangePasswordService.swift
+//  Trackzia
+//
+//  Created by Sushant Alone on 13/12/18.
+//  Copyright © 2018 Private. All rights reserved.
+//
+
+import Foundation
+import ApiManager
+
+class ChangePasswordService : CommunicationEndPoint {
+    var urlPath: String{
+        return "http://13.233.18.64:1166/api/Account/ResetPassword"
+    }
+    
+    var httpMethod: HTTPMethod {
+        return .post
+    }
+    
+    var hearders: HTTPHeaders {
+        return [:]
+    }
+    
+    var parameters: Parameters? {
+        return [:]
+    }
+    
+    var operationId: Int {
+        return 5
+    }
+    
+    var listener: CommunicationResultListener
+    var mobile:String
+    var oldPassword:String
+    var newPassword:String
+    
+    func parseResponse(withOperationId operationId: Int, andStatusCode: Int, data: Data) throws -> CommunicationOperationResult {
+        do {
+            let decoder = JSONDecoder()
+            let confirmPassword = try decoder.decode(ConfirmPassword.self, from: data)
+            return confirmPassword
+        } catch let jsonParsingError {
+            fatalError(jsonParsingError.localizedDescription)
+        }
+    }
+    
+    init(withMobileNumber mobile:String, withOldPassword oldPassword:String, andNewPassword newPassword:String, andListner listener:CommunicationResultListener) {
+        self.mobile = mobile
+        self.oldPassword = oldPassword
+        self.newPassword = newPassword
+        self.listener = listener
+    }    
+}
+
+struct ConfirmPassword: CommunicationOperationResult, Codable {
+    var message: String
+    var success: Bool
+    
+    private enum CodingKeys: String, CodingKey {
+        case message = "Message"
+        case success = "Success"
+    }
+    
+    func encode(to encoder: Encoder) throws
+    {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(message, forKey: .message)
+        try container.encode(success, forKey: .success)
+    }
+    
+}
