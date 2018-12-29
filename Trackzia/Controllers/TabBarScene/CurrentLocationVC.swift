7ef3a8b319ebf2b2be23ff5925f8915adfd07dcf
sushant2030 <sushantalone@Sushants-Mac-mini.local>
2018-12-29 19:54:14 +0530

    Setup For Current Location And multiple Location

--- Trackzia/Controllers/TabBarScene/CurrentLocationVC.swift
+++ Trackzia/Controllers/TabBarScene/CurrentLocationVC.swift
@@ -7,13 +7,61 @@
 //
 
 import UIKit
+import MapKit
 
 class CurrentLocationVC: UIViewController {
     @IBOutlet var liveButton: UIButton!
-    
+    @IBOutlet var mapView: MKMapView!
+    var region:MKCoordinateRegion!
     override func viewDidLoad() {
         super.viewDidLoad()
         liveButton.layer.cornerRadius = 7.0
+        region = mapView.region
+    }
+    
+    private func registerDelegatesSetupViews() {
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+    }
+    
+    func setupViewWithLatAndLong(lat:Float,long:Float){
+        region.center = CLLocationCoordinate2DMake(CLLocationDegrees(lat), CLLocationDegrees(long))
+        region.span.longitudeDelta /= 1.0
+        region.span.latitudeDelta /= 1.0
+        mapView .setRegion(region, animated: false)
+        
+    }
+    
+    func setupMapWIthMultipleAnnotations(locations:[LocationDetailModel]) {
+        let annotations = locations.map { location -> MKAnnotation in
+            let annotation = MKPointAnnotation()
+            annotation.title = location.title
+            let lat = Double(location.latitude);
+            let long = Double(location.longitude)
+            annotation.coordinate = CLLocationCoordinate2D(latitude: lat ?? 0.0, longitude: long ?? 0.0)
+            return annotation
+        }
+        mapView.addAnnotations(annotations)
+    }
+}
+
+extension CurrentLocationVC : MKMapViewDelegate {
+    func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
+        
+    }
+    
+    func mapViewWillStartRenderingMap(_ mapView: MKMapView) {
+        
+    }
+    func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+        
+    }
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        
     }
 }
 
---
+++ Trackzia/Models/LocationDetailModel/LocationDetailModel.swift
@@ -0,0 +1,16 @@
+//
+//  LocationDetailModel.swift
+//  Trackzia
+//
+//  Created by Sushant Alone on 29/12/18.
+//  Copyright © 2018 Private. All rights reserved.
+//
+
+import Foundation
+
+struct LocationDetailModel {
+    let latitude:String!
+    let longitude:String!
+    let title:String!
+    let address:String!
+}
